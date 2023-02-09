bb258cd640789f313a8ceef0785fa252d7897d0a
Oliver Stalder <ostalder@outlook.com>
2023-02-09 17:34:16 +0100

    General cleanup, no substantial changes

--- ic+elspec.py
+++ ic+elspec.py
@@ -3,14 +3,14 @@ import ic4elspec
 import time
 import sys
 
-if sys.platform == 'darwin':
-    matlabroot_dir = "/Applications/MATLAB_R2022b.app/bin/./matlab"
-
+matlabroot_dir = "/Applications/MATLAB_R2022b.app/bin/./matlab"
 if sys.platform == 'linux2':
     matlabroot_dir = "/usr/local/bin/matlab"
 
 cwd = os.getcwd()
 print('Current working Dir: ', cwd)
+
+call_matlab = matlabroot_dir + " -sd \"" + cwd + "/ELSPEC-2022\" -batch "
 
 #check if plotting folder exists:
 if not os.path.isdir('log/testing/plots'):
@@ -20,23 +20,15 @@ if not os.path.isdir('log/testing/plots'):
 #start model
 i = 0
 start_t = time.time()
-os.system(matlabroot_dir + " -sd \"" + cwd + "/ELSPEC-2022\" "+
-          "-batch \"ElSpec_IC\" -nodisplay")
+os.system(call_matlab + "\"ElSpec_IC\" -nodisplay")
+ic4elspec.ic('log/testing/', 'ElSpec-iqt_IC_', i)
 
-ic4elspec.ic('log/testing/',
-             'ElSpec-iqt_IC_', i)
 i = 1
 print('First Iteration:', time.time() - start_t, 's')
 
 while True:
-    osstr = matlabroot_dir + " -sd \"" + cwd + "/ELSPEC-2022\" "+\
-            "-batch \"ElSpec_IC_iter("+str(i)+")\" -nodisplay"
-    os.system(osstr)
+    os.system(call_matlab + "\"ElSpec_IC_iter("+str(i)+")\" -nodisplay")
     ic4elspec.ic('log/testing/',
                  'ElSpec-iqt_IC_', i)
     i = i+1
     print('Mean Iteration Duration:', (time.time() - start_t)/i, 's')
-
-
-
-
