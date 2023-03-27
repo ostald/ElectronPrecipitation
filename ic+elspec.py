7d9f94d8a439aed9fd9d4b7bbb79243d327bffe7
Oliver Stalder <ostalder@outlook.com>
2023-03-27 13:33:09 +0200

    relative paths; remove alpha_eff; remove ElSpec_iqt
    - use relative paths for configurations files to allow for easier portation
    - alpha_eff from IC removed, use native alpha_eff from elspec, as it has more options
    - assimilate ElSpec_iqt and ElSpec_iqt_ic

--- ic+elspec.py
+++ ic+elspec.py
@@ -7,19 +7,19 @@ import loadmat
 
 
 #defining file paths
-msis_config = '/Users/ost051/Documents/PhD/Electron Precipitation/Data/other/msis.rtf'
-iri_config  = '/Users/ost051/Documents/PhD/Electron Precipitation/Data/other/iri.txt'
-chemistry_config = '/Users/ost051/Documents/PhD/Electron Precipitation/Data/other/Reaction rates full set.txt'
-path_eiscat_data = '/Users/ost051/Documents/PhD/Electron Precipitation/Data/Eiscat'
+msis_config = 'Data/other/msis.rtf'
+iri_config  = 'Data/other/iri.txt'
+chemistry_config = 'Data/other/Reaction rates full set.txt'
+path_eiscat_data = 'Data/Eiscat'
 
 mixf = 1
 
 #setup log directory
-setup_ = setup.setup(msis_config, iri_config, chemistry_config, path_eiscat_data, mixf, no_timecode=True)
+setup_ = setup.setup(msis_config, iri_config, chemistry_config, path_eiscat_data, mixf)#, no_timecode=True)
 log_directory = setup_._log_directory
 
 matlabroot_dir = "/Applications/MATLAB_R2022b.app/bin/./matlab"
-if sys.platform == 'linux2':
+if sys.platform == 'linux':
     matlabroot_dir = "/usr/local/bin/matlab"
 
 cwd = os.getcwd()
